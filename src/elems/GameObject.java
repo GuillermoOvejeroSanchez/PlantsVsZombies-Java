91e55c4ff528952c74334f125a992ffbfce56b0f
Marlon <marlonjc@ucm.es>
2018-11-15 12:49:28 +0100

    pepino

--- src/elems/GameObject.java
+++ src/elems/GameObject.java
@@ -8,10 +8,23 @@ public abstract class GameObject {
 	int cycles;
 	int cyclesLeft;
 	String name;
-	
-	public GameObject(int x, int y,int cycles, String name) {
+
+	public GameObject(int x, int y, int cycles, String name) {
 		this.x = x;
 		this.y = y;
 		this.cyclesLeft = cycles;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public boolean update() {
+
+		return true;
 	}
 }
