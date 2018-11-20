a678ae9880210ce985c6d2e9b4c96aa4e60b275c
Marlon <marlonjc@ucm.es>
2018-11-20 19:10:45 +0100

    ahora hereda de comando
    porque le pasamos otro parametro (debug o release)

--- src/commands/PrintModeCommand.java
+++ src/commands/PrintModeCommand.java
@@ -2,13 +2,25 @@ package commands;
 
 import src.Controller;
 import src.Game;
+import print.*;
 
-public class PrintModeCommand extends NoParamsCommand {
+public class PrintModeCommand extends Command{
 
+	
+		
+	
 	public final static String commandName = "print";
 	public final static String commandInfo = "[P]rint";
 	public final static String helpInfo = "change printmode [Release|Debug].";
-
+	
+	
+	private String modo; 
+	
+	
+	public PrintModeCommand() {
+		super(commandName, commandInfo, helpInfo);
+	}
+/*
 	public PrintModeCommand(String nombreComando, String textoInfo, String textoAyuda) {
 		super(nombreComando, textoInfo, textoAyuda);
 
@@ -18,11 +30,33 @@ public class PrintModeCommand extends NoParamsCommand {
 		super(commandName, commandInfo, helpInfo);
 	}
 
+*/
+
 	@Override
 	public void execute(Game game, Controller controller) {
 		System.out.println("Modo de Print cambiado");
-		// TODO execute swapPrintMode();
+		
+		if(modo.equals("DEBUG")) {
+			
+		}
+		else if(modo.equals("RELEASE")) {
+			
+		}
+		
 
+	}
+
+	@Override
+	public Command parse(String[] comando, Controller controller) {
+		Command c = null; 
+		
+		if(comando.length == 2 && (comando[0].equals(commandName) || comando[0].equals(commandName.substring(0, 1)))) {
+			
+			modo = comando[1]; 
+		}
+				
+				
+		return null;
 	}
 
 }
